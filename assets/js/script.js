29426f5f25180833b799f1a926452321597eb5e7
thyago_lbt <thyago14@outlook.fr>
2019-10-31 11:38:00 +0100

    fin

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+function colorchange(){
+  document.getElementById('text').style.color = 'green';
+}
+function colorchange1(){
+  document.getElementById('text').style.color = 'red';
+}
+function colorchange2(){
+  document.getElementById('text').style.color = 'blue';
+}
